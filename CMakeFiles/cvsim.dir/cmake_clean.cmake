b9aaea34a089222d50bc12b86f6598fd9db7e8e5
Riley <ryeakle@gmail.com>
2013-04-13 17:11:03 -0700

    returned CMakeFiles temporarily, needs fix...

---
+++ CMakeFiles/cvsim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "srv_gen"
+  "src/CamTrap_Viper/srv"
+  "CMakeFiles/cvsim.dir/src/node_cvsim/node_cvsim.cpp.o"
+  "CMakeFiles/cvsim.dir/src/node_cvsim/cv_localizer.cpp.o"
+  "bin/cvsim.pdb"
+  "bin/cvsim"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cvsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
