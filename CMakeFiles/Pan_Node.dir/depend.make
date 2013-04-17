265877a1b17f0824c6daa56fc9fdba83d922048f
Riley <ryeakle@gmail.com>
2013-04-17 12:20:43 -0700

    removing unnecessary build files from the repo -Riley

--- CMakeFiles/Pan_Node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Pan_Node.
-# This may be replaced when dependencies are built.
--- devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/viki/groovy_workspace/CamTrap_Viper/devel/setup.sh"
