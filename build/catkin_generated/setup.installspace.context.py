6054a9ecc3891b9fdb24881140361ec708e448c0
Riley <ryeakle@gmail.com>
2013-04-17 12:33:34 -0700

    Almost clean now, removing the last non-source files -Riley

--- build/catkin_generated/setup.installspace.context.py
+++
@@ -1,7 +0,0 @@
-import os;
-CATKIN_GLOBAL_BIN_DESTINATION="bin"
-CATKIN_GLOBAL_LIB_DESTINATION="lib"
-CMAKE_PREFIX_PATH=os.pathsep.join("/home/viki/catkin_ws/devel;/opt/ros/groovy".split(';'))
-PYTHON_EXECUTABLE="/usr/bin/python"
-PYTHON_INSTALL_DIR="lib/python2.7/dist-packages"
-SETUP_DIR="/usr/local"
