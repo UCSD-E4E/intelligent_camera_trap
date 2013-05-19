83209a7af9121a2bd9f325f3cbf370df0c462e7c
mgasaway <mgazaway07@gmail.com>
2013-05-18 22:33:35 -0300

    Create Interpolation.h
    header file added

---
+++ Interpolation/Interpolation.h
@@ -0,0 +1,20 @@
+#ifndef INTERPOLATION_H
+#define INTERPOLATION_H
+
+/******************/
+//global variables//
+/******************/
+
+#define R 4
+#define C 16
+#define polinomialApprox 0
+#define sensorVariation 2.0
+#define threshold 1
+
+#define DEBUG = 0;
+#define inputPixels R*C
+#define outRow ((R-1)*5+1)
+#define outCol ((C-1)*5+1)
+#define outputPixels ((R-1)*5+1)*((C-1)*5+1)
+
+#endif
