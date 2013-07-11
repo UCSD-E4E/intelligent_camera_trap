503b5bc6c46570607c49e63549809b16a84b3725
Gabrielle Trotter <trotter.gabrielle@gmail.com>
2013-07-11 15:31:26 -0700

    interrupt working with trailmaster

--- Hypervisor/atmega/main.c
+++ Hypervisor/atmega/main.c
@@ -5,41 +5,39 @@
  *      Author: Gabrielle Trotter
  */
 
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <avr/sleep.h>
 
-
-
 void set_interrupt_pins();
 void blink_5_times();
+void low_pwr_sleep();
 
-int main (void)
-{
-	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+int main(void) {
 	set_interrupt_pins();
-	//MCUCR &= ~(_BV(PUD));
 
-	//set all pins in port B as output (1)
-	DDRB |= 0xFF;
+	//set only pin 0 of port B as output (1)
 
-	while(1){
+
+	while (1) {
 
 		blink_5_times();
-		sleep_enable();
-		sleep_mode();
+		low_pwr_sleep();
+
 	}
 }
 
-void set_interrupt_pins()
-{
-	DDRD &= 0xFC;     // Clear the PD2 pin
+void set_interrupt_pins() {
+	DDRD &= 0xFD; // Clear the PD2 pin
 	// PD2 (PCINT0 pin) is now an input
 
-	PORTD |= 0x02;    // turn on the Pull-up
-	// PD2 is now an input with pull-up enabled (supposedly)
+	/*
+	 * PORTD |= _BV(PORTD2);    // turn on the Pull-up
+	 *PD2 is now an input with pull-up enabled
+	 *not sure this is necessary because of the pulse we are getting
+	 *from trailmaster stays low then goes high...
+	 */
 
 	// set INT0 edge trigger
 	// 0 = low level, 1 = any, 2 = rising, 3 = falling
@@ -50,25 +48,20 @@ void set_interrupt_pins()
 	EIMSK |= (1 << INT0);
 
 	// turn on global interrupts
-	sei();
 }
 
-
 //Interrupt service routine for pin INT0
-ISR(INT0_vect)
-{
+ISR(INT0_vect) {
 	sleep_disable();
 	PORTB = 0x00;
 	_delay_ms(2000);
 
-
 }
 
-void blink_5_times()
-{
+void blink_5_times() {
+	DDRB = 0xFF;
 	int x = 10;
-	while(x)
-	{
+	while (x) {
 		PINB = _BV(PINB0);
 		_delay_ms(500);
 		x--;
@@ -77,3 +70,12 @@ void blink_5_times()
 
 	PORTB = 0x01;
 }
+
+void low_pwr_sleep() {
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	sleep_enable();
+	sleep_bod_disable();
+	sei();
+	sleep_mode();
+}
+
