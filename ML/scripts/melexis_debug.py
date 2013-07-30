29658bfea62756e1219546ae34d23f4ef705b1cc
SeaLab <sealab@sealabFLIR.(none)>
2013-07-30 11:43:56 -0700

    Add some statistics to bottom of visualizer.  Add command line argument option to save .csv file and pygame frames.

--- ML/scripts/melexis_debug.py
+++ ML/scripts/melexis_debug.py
@@ -1,82 +1,86 @@
-# MLX Viewer
-# Displays the output of the MLX90620 thermal sensor in a human-friendly format
-# Creates a 5 min video file of the output
-# Creates a CSV file of the sensor values during the video
-#
-# Authors: Riley Yeakle, Samuel Chen, Ali Khodamoradi, David Muller, Gabrielle Trotter
+#!/usr/bin/env python
+# Grab the 64 pixel "temperatures" from MLX90620. 
+# "-s" command line argument will save a .csv file, and time-stamped pygame frames 
+# (.csv file will be: amibent_temp,pixel_1, pixel_2, pixel_3,...,pixel_64,time_stamp)
+# directories for these files modifiable at beginning at script.
 
+
+import numpy as np
 import fnmatch
 import sys
 import serial
 import Image
 import pygame
-import os
 import time
 from time import localtime, strftime
+import traceback
+import os
+import rospy
 
-# Parameters
-CELL_SIZE = 30
-MAX_TEMP = 40
-MIN_TEMP = 20
-VIDEO_DIRECTORY = '/home/sealab/Videos/IRVideo/'	
-VIDEO_LEN = 1
-# Cell:
-# Used to render sensor readings on the pygame window
-# Create one per sensor pixel.
-# Example:
-#  myCell.render(255, myScreen)
-#  > Creates a white square with side length = size pixels 
-#    at the cell's x and y coordinates on the pygame screen
+
+from std_msgs.msg import String
+
+
 class Cell:
 	x = 0
 	y = 0
 	size = 15
-	intensity = 0;
+
 	def __init__(self, pos_x, pos_y, size):
 		self.x = pos_x
 		self.y = pos_y
 		self.size = size
-	def render(self, intensity, screen):
-		self.intensity = intensity
-		color = [intensity, intensity, intensity]
-		pygame.draw.rect(screen, color, (self.x, self.y, self.size, self.size), 0)	
+	def render(self, intensity, value, screen):
+		font = pygame.font.SysFont("times", 15,bold=True)				
+		col = [intensity, intensity, intensity]
+		rect = pygame.draw.rect(screen, col, (self.x, self.y, self.size, self.size), 0)
+		text = font.render(str(value),1, (255,0,0))
+		screen.blit(text,rect,special_flags=0)	
 
 
-# temperatureMap
-# arguments: vector of temperature values, low_temp, high_temp 
-# returns: vector of greyscale pixel intensities [0, 255]
-# Example Use: (data = [1, 2, 3])
-# temperatureMap(data, 1, 3)
-# > [0, 127.5, 255]
-def temperatureMap(values, low_temp, high_temp):
-	scale_factor = 255.0/high_temp
-	for i in range(len(values)):
-		values[i] = (values[i] - low_temp) * scale_factor
-		if values[i] > 255:
-			values[i] = 255
-		if values[i] < 0:
-			values[i] = 0
-	return values
 
-## START SCRIPT
+#display some stats on screen
+def renderStats(mean_frame_temp, frame_std_dev, high_frame_temp, low_frame_temp, ambient_temp):
+	#format all the floats	
+	mean_frame_temp_str = str.ljust("Mean temp of this frame",48) + " = %.2f" %mean_frame_temp
+	frame_std_dev_str = str.ljust("Std Deviation of this frame",48) + " = %.2f" % frame_std_dev
+	high_frame_temp_str = str.ljust("High temp of this frame",48) + " = %.1f" % high_frame_temp
+	low_frame_temp_str = str.ljust("Low temp of this frame",48) + " = %.1f" % low_frame_temp	
+	ambient_temp_str = str.ljust("Ambient temp of this frame",46) + " = %.1f" %  ambient_temp	
+
+	myfont = pygame.font.SysFont("times", 18)
+	
+	#generate and display all labels
+	mean_frame_temp_label = myfont.render(mean_frame_temp_str, 1, (255,255,255))
+	screen.blit(mean_frame_temp_label, (0,(CELL_SIZE * row)))
+
+	frame_std_dev_label = myfont.render(frame_std_dev_str, 1, (255,255,255))
+	screen.blit(frame_std_dev_label, (0,(CELL_SIZE * (row+1))))
+
+	high_frame_temp_label = myfont.render(high_frame_temp_str, 1, (255,255,255))
+	screen.blit(high_frame_temp_label, (0,(CELL_SIZE * (row+2))))
+
+	low_frame_temp_label = myfont.render(low_frame_temp_str, 1, (255,255,255))
+	screen.blit(low_frame_temp_label, (0,(CELL_SIZE * (row+3))))
+
+	ambient_temp_label = myfont.render(ambient_temp_str, 1, (255,255,255))
+	screen.blit(ambient_temp_label, (0,(CELL_SIZE * (row+4))))
+
+
+
+
+
+csv_file_directory = "/home/viki/Documents/IRVideo/IR_CSV_Files/"	
+IR_images_directory = "/home/viki/Videos/IRVideo/imgtemp/"
+if len(sys.argv) >= 2:
+	save_file_boolean = sys.argv[1]
+else: 
+	save_file_boolean = "False"
+
 ser = serial.Serial()
-rate = 0
-
-#check for command line arguments
-num_args = len(sys.argv)
-if num_args == 3:
-	br_str = fnmatch.filter(sys.argv, 'b*')
-	port_str = fnmatch.filter(sys.argv, 'tty*')[0]
-	rate = int(br_str[0].replace("b", ""));
-elif num_args == 1:
-	rate = 115200
-	port_str = "ttyACM0"
-else:	
-	print ("Example Usage: python melexis_debug.py ttyACM0 b115200")
-	exit()
-
-ser.port = "/dev/" + port_str
-
+ser.port = "/dev/ttyACM0"
+rate = 115200
+	
 print ("Opening port:" + ser.port + " at b" + str(rate))
 
 #Set Serial Properties
@@ -87,9 +91,12 @@ ser.stopbits = serial.STOPBITS_ONE
 ser.timeout = 2 
 ser.writeTimeout = 2
 
+interpolationScaling = 1
+row_Original = 4
+column_Original = 16
+row_Effective = (row_Original - 1) * interpolationScaling + 1
+column_Effective = (column_Original - 1) * interpolationScaling + 1
 
-row = 4
-column = 16
 
 try: 
 	ser.open()
@@ -99,57 +106,47 @@ except Exception, e:
 	exit()
 
 if ser.isOpen():
-	
-	imgcnt = 0
-	movcnt = 0
+	cell_size = 50
 	
 	#initialize pygame display 
+	#grid[i].render(data_vector[(i % row_Effective) * column_Effective + (i / 	row_Effective)])				
 	pygame.init()
 	white = [255,255,255]
-	size = [CELL_SIZE*column, CELL_SIZE*row]
+	size = [cell_size*column_Effective, cell_size*(row_Effective+5)]
 	screen = pygame.display.set_mode(size)
 	screen.fill(white);
 	grid = [];
-	
-	for i in range(column -1, -1, -1):
-		for j in range(row):
-			newCell = Cell(i*CELL_SIZE, (j)*CELL_SIZE, CELL_SIZE)
+		
+	#q = column_Effective - 1
+	for q in range(column_Effective-1, -1, -1):
+	#while (q):
+		for j in range(row_Effective): 
+			newCell = Cell(q*cell_size, (j)*cell_size, cell_size)
 			grid.append(newCell)			
-			
+	#	q -= 1
+		
 	for i in range(len(grid)):
-		grid[i].render(127.6, screen)
-
+		grid[i].render(127.6, 0,screen)
+	
 	pygame.display.flip()
 	try:
 		#clean out buffers
 		ser.flushInput()
 		ser.flushOutput()
-		
+			
 		#make sure we read both the ambient and the data each cycle
 		read_amb = False
 		read_dat = False
 		
-
-		#create initial CSV file with a new start time
-		startTime = time.time()
-		fileName = strftime("%m-%d-%H:%M:%S", localtime())
-		csvFile = open(VIDEO_DIRECTORY + "IR_CSV_FILES/" + fileName + ".csv", 'a+')
-		
-		while True:		
-			currentTime = time.time()
-			#generate a new video file and CSV file every VIDEO_LEN minutes
-			if (currentTime - startTime) >= (60. * VIDEO_LEN):
-				outstr = 'ffmpeg -f image2 -r 5 -i ' + VIDEO_DIRECTORY + "imgtemp/%d.jpeg -vcodec mpeg4 -y " + VIDEO_DIRECTORY + fileName + '.mp4'
-				os.system(outstr)
-				clearImages = "rm " + VIDEO_DIRECTORY + 'imgtemp/*.jpeg'
-				os.system(clearImages)
-				imgcnt = 0
-				#create new file name, new csv file, and reset start time
-				fileName = strftime("%m-%d-%H:%M:%S", localtime())
-				csvFile = open(VIDEO_DIRECTORY + "IR_CSV_FILES/" + fileName + ".csv", 'a+')
-				startTime = time.time()
+		current_time = strftime("%m-%d-%H:%M:%S",localtime())	
+		csvFileName = current_time
+		while not rospy.is_shutdown():			
 			response = ser.readline()
-			if response.startswith("Ambient"):
+			current_time = strftime("%m-%d-%H:%M:%S",localtime())			
+
+			#add len(response) <= 16 to address bug where sometimes we read lines like this:
+			# Ambient.6 24.5 23.7 23.8 25.4 24.4 24.7 24.6 24.5 24.9 25.1 23.2
+			if response.startswith("Ambient") and len(response) <=16: 
 				ambient = float(response.strip('Ambient T='))
 				read_amb = True
 			elif response.startswith("IR:"):
@@ -157,36 +154,67 @@ if ser.isOpen():
 				read_dat = True
 	
 			if read_amb and read_dat:
-				#transform input string into float array
+				#we've had errors where the mbed reports back something like 33..1
+				#so, make sure everything will convert to float first
 				try:
 					data_list = data.split()
-					data_vector = map(float, data_list)
+					data_vector = map(float, data_list) 
 				except:
-					print "Error in sensor value string to float conversion"
-					continue 
-				if len(data_vector) == row * column:	
-					scaled_data = temperatureMap(data_vector, MIN_TEMP, MAX_TEMP)
-				
-					ambientTemp = str(ambient)
-					csvFile.write(ambientTemp)	#start CSV file with ambient temp
-					csvFile.write(',')
-					for i in range(len(scaled_data)):
-						grid[i].render(scaled_data[i], screen) 	#display a pixel
+					print "Could not convert IR data to float."					
+					continue
+	
+				if len(data_vector) == 64: #make sure we got 64 pixels
+			 		#compute some stats						
+					mean_frame_temp = np.mean(data_vector)
+					frame_std_dev = np.std(data_vector)
+					high_frame_temp = max(data_vector)
+					low_frame_temp = min(data_vector)
+					ambient_temp = ambient
 
-						value = str(data_vector[i])
-						csvFile.write(value + ',') 	#print the rest of the values to the CSV file
-					
-					csvFile.write('\n')	#CSV new line:
+					renderStats(mean_frame_temp, frame_std_dev, high_frame_temp, low_frame_temp, ambient_temp)
+
+					#save image .csv file and image frame if given "-s" cmd line arg
+					if save_file_boolean == "-s":
+						#write in this row of the .csv file			
+						csvFile = open(csv_file_directory+csvFileName+".csv", 'a+')
+						ambientTemp = str(ambient)				
+						csvFile.write(ambientTemp)   #start with ambient temp
+						csvFile.write(',')
+						for i in range(0, len(data_vector)):
+							value = str(data_vector[i])					
+							csvFile.write(value)
+							csvFile.write(',')
+						csvFile.write(current_time) #time stamp
+						csvFile.write(',')
+						csvFile.write('\n') 		   #we're done with this row
+						csvFile.close()
+
+						pygame.image.save(pygame.display.get_surface(), IR_images_directory+current_time+'.jpeg')  
+
+					data_vector_copy = list(data_vector) #preserve original temp values					
+
+					#assign colors for cells
+					for i in range(len(data_vector)):
+						data_vector[i] -= 10
+						data_vector[i] *=  (150/(40-5))
+						if data_vector[i] > 255:
+							data_vector[i] = 255
+						if data_vector[i] < 0:
+							data_vector[i] = 0
+				
+					#display those colors
+					for i in range(len(grid)):
+						grid[i].render(data_vector[i], data_vector_copy[i], screen)
 	
 					pygame.display.flip()
-					imgcnt += 1
-					pygame.image.save(pygame.display.get_surface(), VIDEO_DIRECTORY + 'imgtemp/' +str(imgcnt) + '.jpeg')
-
 					read_amb = False
 					read_dat = False 
-			
+				
 	except Exception, e1:
 		print "Error in communication with port: " + str(e1)
+  		traceback.print_exc(file=sys.stdout)  #print stack trace
 
 else:
 	print "Cannot open serial port"	
+
+
