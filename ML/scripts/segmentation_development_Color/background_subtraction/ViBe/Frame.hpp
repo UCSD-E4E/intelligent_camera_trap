5c8137b6d60d943281fde7eb0d7665e6360b4742
David Muller <davehmuller@gmail.com>
2013-08-06 16:06:27 -0700

    Added a rough draft C++ implementation of ViBe background subtraction algorithm.

---
+++ ML/scripts/segmentation_development_Color/background_subtraction/ViBe/Frame.hpp
@@ -0,0 +1,12 @@
+#ifndef FRAME_HPP_
+#define FRAME_HPP_
+
+//#define VIDEO_WIDTH 134
+//#define VIDEO_HEIGHT 100
+
+typedef struct Frame{
+    int picture[134][100];
+} Frame;
+
+
+#endif /* FRAME_HPP_ */
