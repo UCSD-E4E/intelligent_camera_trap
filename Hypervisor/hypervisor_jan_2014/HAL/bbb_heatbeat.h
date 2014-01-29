57a75256d9615b75ea7cf11302bf382d17d19428
muhsingurel <muhsingurel@gmail.com>
2014-01-29 00:35:30 -0800

    Update bbb_heatbeat.h
    readBBBHeartBeatLine(void)function added

--- Hypervisor/hypervisor_jan_2014/HAL/bbb_heatbeat.h
+++ Hypervisor/hypervisor_jan_2014/HAL/bbb_heatbeat.h
@@ -5,5 +5,6 @@ unsigned char get_bbb_heart_beat(void);
 void set_bbb_heart_beat(unsigned char currentHearBeat);
 unsigned int get_bbb_heart_beat_inactive_time_in_S(void);
 void increment_bbb_heart_beat_inactive_time_100MS(void);
+extern unsigned char readBBBHeartBeatLine(void);
 
 #endif
