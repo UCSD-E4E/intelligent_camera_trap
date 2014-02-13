154d1a407fa08bd04dc3a40c0050407ed593c140
muhsingurel <muhsingurel@gmail.com>
2014-02-12 19:04:32 -0800

    Update port.c

--- Hypervisor/hypervisor_jan_2014/HAL/port.c
+++ Hypervisor/hypervisor_jan_2014/HAL/port.c
@@ -3,36 +3,36 @@
 void PORT_Init()
 {
 	
-	DDRA = 0b00000000;
-	PORTA = 0b00000100;
+	DDRA = 0b00000111; //set Relay as output (Bit0:2 = 1), Main Power, GoPro Power and GoPro On/Off Control Output
+	PORTA = 0b00000000; 
 	
 
-	DDRB = 0b00001111;
-	PORTB = 0b00000000;//set Relay as output (Bit0:3 = 1)
-			
+	DDRB = 0b00000000;
+	PORTB = 0b00000100; // INT2 pull-up Trail Master Input
+	
 
-	DDRC = 0b00000000;
-	PORTC = 0b00000001;
+	DDRC = 0b10000000; // BBB Power Control Output
+	PORTC = 0b00000000;
 	
 
 	DDRD = 0b00000000;
-	PORTD = 0b00000100;//set TX as output (Bit1 = 1)
-			
+	PORTD = 0b00111100;//set TX as output (Bit1 = 1) BBB Heartbeat and Shutdown Input 
+	
 }
 
 unsigned char inputON1()
 {
-	return !(PINA & 0b00000100) ;
-}	
+	return !(PIND & 0b0000100) ;
+}
 
 unsigned char inputON2()
 {
-	return !(PINA & 0b00000010) ;
+	return !(PIND & 0b00001000) ;
 }
 
 unsigned char inputON3()
 {
-	return !(PINC & 0b00000001) ;
+	return !(PIND & 0b00010000) ;
 }
 
 unsigned char inputON4()
@@ -44,11 +44,11 @@ void relayON1(unsigned char cmd)
 {
 	if (cmd)
 	{
-		PORTB = PORTB | 0b00000001;		
+		PORTA = PORTA | 0b00000001;
 	}
 	else
 	{
-		PORTB = PORTB & 0b11111110;
+		PORTA = PORTA & 0b11111110;
 	}
 	return ;
 }
@@ -57,11 +57,11 @@ void relayON2(unsigned char cmd)
 {
 	if (cmd)
 	{
-		PORTB = PORTB | 0b00000010;
+		PORTA = PORTA | 0b00000010;
 	}
 	else
 	{
-		PORTB = PORTB & 0b11111101;
+		PORTA = PORTA & 0b11111101;
 	}
 	return ;
 }
@@ -70,11 +70,11 @@ void relayON3(unsigned char cmd)
 {
 	if (cmd)
 	{
-		PORTB = PORTB | 0b00000100;
+		PORTA = PORTA | 0b00000100;
 	}
 	else
 	{
-		PORTB = PORTB & 0b11111011;
+		PORTA = PORTA & 0b11111011;
 	}
 	return ;
 }
@@ -84,12 +84,11 @@ void relayON4(unsigned char cmd)
 {
 	if (cmd)
 	{
-		PORTB = PORTB | 0b00001000;
+		PORTC = PORTC | 0b10000000;
 	}
 	else
 	{
-		PORTB = PORTB & 0b11110111;
+		PORTC = PORTC & 0b01111111;
 	}
 	return ;
 }
-
