5ad4a4742220cf74a2a61f780887b437082ca0ac
DavidMuller <davehmuller@gmail.com>
2014-03-11 01:25:16 +0000

    Remove turret development code

--- Turret/NINJA_168/NINJA_168/src/Timer.h
+++
@@ -1,6 +0,0 @@
-extern void setTimerIn4ms(unsigned int timerSetting);
-extern void decrementTimerBy4ms(void);
-extern unsigned int getTimerIn100ms(void);
-extern unsigned char timerExpired(void);
-extern void TIMER_Init(void);
-
